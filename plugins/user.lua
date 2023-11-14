7d4bad476e9d3aab0550a124cb87b942d6433262
Oleksii Kalinin <oleksiikalinin@pingidentity.com>
2023-11-14 16:49:22 +0100

    update

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,31 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "User AstroFile",
+    cmd = "TodoQuickFix",
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODO in Telescope" },
+    },
+  },
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+      },
+    },
+  },
 }
